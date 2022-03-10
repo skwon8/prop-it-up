7a3d56b09e20da8fc69d51eabce5b05b2e6ab850
skwon8 <saengsukwon@gmail.com>
2022-03-09 19:14:21 -0500

    Putting it Together Core Assignment using propitup

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="App">
       <h1>Person Card!</h1>
       <hr />
-      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {"45"} hairColor = {"Black"}></PersonCard>
+      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}></PersonCard>
 
-      <PersonCard firstName = {"John"} lastName = {"Smith"} age = {"88"} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"}></PersonCard>
 
-      <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {"50"} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"}></PersonCard>
 
-      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {"62"} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"}></PersonCard>
     </div>
     
   );
--- src/components/Personcard.jsx
+++ src/components/Personcard.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'; // import react and component so that we can use those to create a class based react component
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+
+        this.buttonHandler = () => {
+            this.setState({age: this.state.age +1})
+        }
+    }
+    
     render() {
         return (
             <div>
                 <h1>{this.props.lastName }, {this.props.firstName}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h3>Age: {this.state.age}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+                <button onClick = {this.buttonHandler}>Birthday Button for {this.props.lastName }, {this.props.firstName} &#9829;</button>
                 <hr />
             </div>
         )
